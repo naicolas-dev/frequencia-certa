d497c3d952c306049eada0fc1286a1e34b35e64b
Nicolas <nicolas.viana.moc@gmail.com>
2026-01-10 09:11:58 -0300

    refactor(api): remove grupo de rotas redundante e sem utilidade

--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,3 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// Grupo de rotas protegidas (Só acessa se tiver logado no App)
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/disciplinas', [DisciplinaApiController::class, 'index']);
-});
-
