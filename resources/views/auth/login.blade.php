85c6a8006b72d9404e5927af7eca4e658cd37a2c
Nicolas <nicolas.viana.moc@gmail.com>
2026-01-13 12:11:02 -0300

    style(ui): padronizar cor de fundo do loader no modo escuro na tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -46,7 +46,7 @@
   >
 
     <div id="page-loader"
-     class="fixed inset-0 z-[9999] bg-[#2D6AE6] flex items-center justify-center flex-col transition-colors duration-500 dark:bg-[#1F4CC7]">
+     class="fixed inset-0 z-[9999] bg-[#2D6AE6] flex items-center justify-center flex-col transition-colors duration-500 dark:bg-[#2D6AE6]">
         <div class="loader-content text-white text-4xl md:text-5xl font-black tracking-tighter opacity-0 translate-y-4"
             style="font-family: 'Instrument Sans', sans-serif;">
             Frequência Certa
