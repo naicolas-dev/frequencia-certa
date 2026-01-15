81406c2acfc5e15207f6c980a04fac89067581b8
Nicolas <nicolas.viana.moc@gmail.com>
2026-01-15 15:53:40 -0300

    chore(landing): substituir avatar externo por ícone inline
    - Remove dependência do ui-avatars.com na mockup
    - Usa SVG local para reduzir requests e melhorar privacidade/Lighthouse

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -468,9 +468,19 @@
                                     <x-application-logo class="w-6 h-6 text-[#1D4ED8] dark:text-[#06B6D4]" />
                                     <span class="font-bold text-[#1E3A8A] dark:text-white tracking-tight text-sm">Frequência Certa</span>
                                 </div>
-                                <div class="w-8 h-8 rounded-full bg-gray-100 dark:bg-gray-800 overflow-hidden border border-gray-200 dark:border-gray-700">
-                                    <img src="https://ui-avatars.com/api/?name=User&background=random" alt="Avatar Usuário" class="w-full h-full opacity-80" width="32" height="32">
-                                </div>
+                                <svg 
+                                    class="w-8 h-8 rounded-full bg-gray-100 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 opacity-80 text-gray-400 dark:text-gray-500 p-1" 
+                                    fill="currentColor" 
+                                    viewBox="0 0 24 24" 
+                                    xmlns="http://www.w3.org/2000/svg" 
+                                    aria-hidden="true"
+                                >
+                                    <path 
+                                        fill-rule="evenodd" 
+                                        d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" 
+                                        clip-rule="evenodd"
+                                    ></path>
+                                </svg>
                             </div>
                             
                             <div class="relative flex-1 px-4 overflow-hidden bg-[#F8FAFC] dark:bg-[#030712] transition-colors">
